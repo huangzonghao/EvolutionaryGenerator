2196c653b9a37fca34f1df898d29cfa17bd6def6
HuangZonghao <coding@hzh.io>
2021-04-20 13:23:41 -0400

    Update api for data flow

--- generator/include/GenerateDemoRobot.h
+++ generator/include/GenerateDemoRobot.h
@@ -2,8 +2,9 @@
 #define GENERATEDEMOROBOT_H_HEJ0DKGV
 
 #include <string>
+#include <vector>
 
-void generate_demo_robot(std::string mode, double scales[7]);
-std::string generate_demo_robot_string(std::string mode, double scales[7]);
+void generate_demo_robot(std::string mode, const std::vector<float>& scales);
+std::string generate_demo_robot_string(std::string mode, const std::vector<float>& scales);
 
 #endif /* end of include guard: GENERATEDEMOROBOT_H_HEJ0DKGV */
