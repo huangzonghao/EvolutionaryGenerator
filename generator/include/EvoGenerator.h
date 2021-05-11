06c439da6193c4ec0fd0d0737a2a25e770afc5dd
HuangZonghao <coding@hzh.io>
2021-05-11 19:09:06 -0400

    Add a class wrapper for the template based classes

--- generator/include/EvoGenerator.h
+++ generator/include/EvoGenerator.h
@@ -1,27 +1,23 @@
 #ifndef EVOGENERATOR_H_MSPHUOQV
 #define EVOGENERATOR_H_MSPHUOQV
 
-#include <sferes/gen/evo_float.hpp>
-#include <sferes/modif/dummy.hpp>
-#include "sferes_fit_EvoGenFitness.hpp"
-#include "sferes_phen_EvoGenPhen.hpp"
-#include "sferes_eval_EvoGenEval.hpp"
-#include "sferes_stat_EvoGenStat.hpp"
-#include "sferes_qd_container_grid.hpp"
-#include "sferes_qd_selector_uniform.hpp"
-#include "sferes_qd_EvoGenQD.hpp"
-#include "sferes_params.h"
+#include <string>
+#include "SimulatorParams.h"
+#include "EvoParams.h"
 
-typedef sferes::phen::EvoGenPhen<sferes::gen::EvoFloat<Params::evo_float::dimension, Params>,
-                                 sferes::fit::EvoGenFitness<Params>,
-                                 Params> phen_t;
+class EvoGenerator {
+  public:
+    void set_evo_params(const EvoParams& evo_params) { evo_params_ = evo_params; }
+    void set_sim_params(const SimulatorParams& sim_params) { sim_params_ = sim_params; }
+    void set_result_dir(const std::string& res_dir) { res_dir_ = res_dir; }
+    void run();
+    void resume() {}
+  private:
+    EvoParams evo_params_;
+    SimulatorParams sim_params_;
+    std::string res_dir_;
+};
 
-typedef sferes::qd::EvoGenQD<phen_t,
-                             sferes::eval::EvoGenEval<Params>,
-                             boost::fusion::vector<sferes::stat::EvoGenStat<phen_t, Params> >,
-                             sferes::modif::Dummy<> modifier_t,
-                             sferes::qd::selector::Uniform<phen_t>,
-                             sferes::qd::container::Grid<phen_t> > EvoGenerator;
 
 
 #endif /* end of include guard: EVOGENERATOR_H_MSPHUOQV */
--- generator/src/evo_main.cpp
+++ generator/src/evo_main.cpp
@@ -1,28 +1,26 @@
-#include <iostream>
-#include <fstream>
 #include <filesystem>
-#include <chrono>
 
 #include "SimulatorParams.h"
-#include "EvoGenerator.h"
 #include "EvoParams.h"
+#include "EvoGenerator.h"
 
 #include "evo_paths.h"
 
 int main(int argc, char **argv)
 {
+    EvoGenerator evo_gen;
     EvoParams evo_params;
     SimulatorParams sim_params;
     evo_params.set_nb_gen(200);
 
-    if (argc == 2) {
-        std::filesystem::path res_path(argv[1]);
-        sim_params.Load(res_path.parent_path().parent_path().string() + "/sim_params.xml");
-        EvoGenerator evo_gen;
-        evo_gen.resume(res_path.string());
-        std::cout << "Done" << std::endl;
-        return 0;
-    }
+    // if (argc == 2) {
+        // std::filesystem::path res_path(argv[1]);
+        // sim_params.Load(res_path.parent_path().parent_path().string() + "/sim_params.xml");
+        // EvoGenerator evo_gen;
+        // evo_gen.resume(res_path.string());
+        // std::cout << "Done" << std::endl;
+        // return 0;
+    // }
 
     // set up output dir
     time_t t = time(0);
@@ -43,22 +41,11 @@ int main(int argc, char **argv)
     sim_params.AddWaypoint(0.5, 1.5, 0.3);
     sim_params.SetCamera(2.5, -1, 3, 2.5, 1.5, 0);
 
-    EvoGenerator evo_gen(evo_params);
-    evo_gen.eval().set_sim_params(sim_params);
-    evo_gen.set_res_dir(log_dir);
-    sim_params.Save(log_dir + "/sim_params.xml");
+    evo_gen.set_evo_params(evo_params);
+    evo_gen.set_sim_params(sim_params);
+    evo_gen.set_result_dir(log_dir);
 
-    std::chrono::duration<double> time_span; // in seconds
-    std::chrono::steady_clock::time_point tik = std::chrono::steady_clock::now();
-    evo_gen.run(argv[0]);
-    time_span = std::chrono::steady_clock::now() - tik;
+    evo_gen.run();
 
-    std::ofstream ofs;
-    ofs.open(log_dir + "/progress.txt", std::ofstream::out | std::ofstream::app);
-    ofs << "Finished in: " << time_span.count() << "s" << std::endl;
-    ofs.close();
-
-    std::cout << "Generation done" << std::endl
-              << "Total run time: " << time_span.count() << "s" << std::endl;
     return 0;
 }
