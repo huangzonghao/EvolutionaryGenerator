3792352f919f78e0aa8b25667876fb6d3471fc70
HuangZonghao <coding@hzh.io>
2021-05-20 14:03:10 -0400

    Update robot generator for the new genotype

--- generator/src/robogami_test.cpp
+++ generator/src/robogami_test.cpp
@@ -8,8 +8,8 @@
 
 int main(int argc, char *argv[]) {
     // protoToUrdf(Robogami_Data_Dir + "/proto/examples/killerBasket_good_servospacing.asciiproto", "killer");
-    std::vector<double> design_vector = {4, 1, 1, 1, 0.625, 1, 0.875, 1, 1.125, 1, 1.375, 1}; // default robot
-    // std::vector<double> design_vector = {4, 1, 1, 1, 0.7, 1, 1.2, 1, 1.1, 1, 0.6, 1};
+    std::vector<double> design_vector = {1, 1, 1, 4, 0.125, 1, 0.1, 1, 0.375, 1,  0.1, 1, 0.625, 1, 0.1, 1, 0.875, 1, 0.1, 1}; // default robot
+    // std::vector<double> design_vector = {1, 1, 1, 4, 0.125, 2, 0.1, 1, 0.4, 1, 0.375, 1,  0.1, 1, 0.625, 1, 0.1, 1, 0.875, 3, 0.1, 1, 0.1, 1, 0.1, 1}; // multi-link test
     generate_demo_robot_file("leg", design_vector, "test_1");
     std::cout << "robogami test done" << std::endl;
     system("pause");
