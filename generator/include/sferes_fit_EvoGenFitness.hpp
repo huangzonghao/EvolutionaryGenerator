5cb55f134831155eba00c6a0bc5e551bda4ff9c4
HuangZonghao <coding@hzh.io>
2021-05-24 16:57:42 -0400

    Use length of genome as feature

--- generator/include/sferes_fit_EvoGenFitness.hpp
+++ generator/include/sferes_fit_EvoGenFitness.hpp
@@ -45,7 +45,9 @@ class EvoGenFitness {
     template <typename Indiv>
     void update_desc(Indiv& ind) {
         _desc[0] = ind.gen().data(0);
-        _desc[1] = ind.gen().data(3);
+        _desc[1] = ind.gen().size() - 10; // lengeth of gen [10, 25]
+        // TODO: better handling the map size
+        _desc[1] = _desc[1] / 20;
     }
 
     template<class Archive>
