186b857c0c4aa9107e289f6a3efc91dc31749355
HuangZonghao <coding@hzh.io>
2021-05-26 13:05:29 -0400

    Add a switch to turn off output

--- generator/src/EvoGenerator.cpp
+++ generator/src/EvoGenerator.cpp
@@ -22,20 +22,25 @@ typedef sferes::qd::EvoGenQD<phen_t,
                              sferes::qd::container::Grid<phen_t> > qd_t;
 
 void EvoGenerator::run() {
+    if (!evo_params_.output_enabled())
+        std::cout << "Note: Output disabled" << std::endl;
     qd_t qd(evo_params_);
     qd.set_res_dir(res_dir_);
     qd.eval().set_sim_params(sim_params_);
-    sim_params_.Save(res_dir_ + "/sim_params.xml");
+    if (evo_params_.output_enabled())
+        sim_params_.Save(res_dir_ + "/sim_params.xml");
 
     std::chrono::duration<double> time_span; // in seconds
     std::chrono::steady_clock::time_point tik = std::chrono::steady_clock::now();
     qd.run("EvolutionGenerator");
     time_span = std::chrono::steady_clock::now() - tik;
 
-    std::ofstream ofs;
-    ofs.open(res_dir_ + "/progress.txt", std::ofstream::out | std::ofstream::app);
-    ofs << "Finished in: " << time_span.count() << "s" << std::endl;
-    ofs.close();
+    if (evo_params_.output_enabled()) {
+        std::ofstream ofs;
+        ofs.open(res_dir_ + "/progress.txt", std::ofstream::out | std::ofstream::app);
+        ofs << "Finished in: " << time_span.count() << "s" << std::endl;
+        ofs.close();
+    }
 
     std::cout << "Generation done" << std::endl
               << "Total run time: " << time_span.count() << "s" << std::endl;
