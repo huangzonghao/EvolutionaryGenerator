61e4b95f1f4aa6e9d0d5f55654936114735f16d4
HuangZonghao <coding@hzh.io>
2021-05-31 12:08:19 -0400

    Mutate num_legs

--- generator/include/sferes_gen_EvoGenFloat.hpp
+++ generator/include/sferes_gen_EvoGenFloat.hpp
@@ -19,6 +19,10 @@ static const double gen_cross_rate = 0.75f;
 static const double gen_eta_c = 10.0f;
 static const double gen_yl = 0.0;
 static const double gen_yu = 1.0;
+static const int max_num_legs = 3;
+static const int min_num_legs = 2;
+static const int max_num_links = 3;
+static const int min_num_links = 1;
 
 namespace evo_float {
 
@@ -26,7 +30,7 @@ namespace evo_float {
 // perturbation of the order O(1/eta_m)
 template<typename Ev>
 struct Mutation_f {
-    void operator()(Ev& ev, size_t i) {
+    double operator()(double raw) {
         assert(gen_eta_m != -1.0f);
         double ri = misc::rand<double>();
         double delta_i = ri < 0.5 ?
@@ -34,8 +38,14 @@ struct Mutation_f {
                          1 - pow(2.0 * (1.0 - ri), 1.0 / (gen_eta_m + 1.0));
         assert(!std::isnan(delta_i));
         assert(!std::isinf(delta_i));
-        double f = ev.data(i) + delta_i;
-        ev.data(i, std::clamp(f, 0.0, 1.0));
+        return std::clamp(raw + delta_i, 0.0, 1.0);
+    }
+    int operator()(int raw) {
+        if (misc::flip_coin())
+            raw += 1;
+        else
+            raw -= 1;
+        return raw;
     }
 };
 
@@ -103,22 +113,70 @@ class EvoGenFloat {
     EvoGenFloat() {}
 
     void mutate() {
+        std::vector<double> tmp_data;
+        // TODO: how to improve memory management?
+        tmp_data.reserve(_data.size());
         // body_x, body_y, body_z
         for (int i = 0; i < 3; ++i) {
             if (misc::rand<double>() < gen_mutation_rate)
-                _mutation_op(*this, i);
+                tmp_data.push_back(_mutation_op(_data[i]));
+            else
+                tmp_data.push_back(_data[i]);
         }
-        // TODO: mutate num_legs/num_links
+        // TODO: mutate num_links
         int num_legs = _data[3];
+        int tmp_num_legs = num_legs;
         int num_links = 0;
-        for (int i = 4; i < _data.size(); i += num_links * 2 + 1) {
-            num_links = _data[i];
-            for (int j = 1; j < num_links * 2 + 1; ++j) {
-                if (misc::rand<double>() < gen_mutation_rate)
-                    _mutation_op(*this, i + j); // link id & link scale
+        int tmp_num_links = 0;
+        // first mutate num_legs
+        if (misc::rand<double>() < gen_mutation_rate)
+            tmp_num_legs = std::clamp(_mutation_op(tmp_num_legs), min_num_legs, max_num_legs);
+        tmp_data.push_back(tmp_num_legs);
+        int cursor = 4;
+        if (num_legs >= tmp_num_legs) {
+            for (int i = 0; i < tmp_num_legs; ++i) {
+                num_links = _data[cursor];
+                tmp_data.push_back(num_links);
+                for (int j = 0; j < num_links * 2; ++j) {
+                    if (misc::rand<double>() < gen_mutation_rate)
+                        tmp_data.push_back(_mutation_op(_data[cursor + j + 1]));
+                    else
+                        tmp_data.push_back(_data[cursor + j + 1]);
+                }
+                cursor += num_links * 2 + 1;
+            }
+        } else {
+            for (int i = 0; i < num_legs; ++i) {
+                num_links = _data[cursor];
+                tmp_data.push_back(num_links);
+                for (int j = 0; j < num_links * 2; ++j) {
+                    if (misc::rand<double>() < gen_mutation_rate)
+                        tmp_data.push_back(_mutation_op(_data[cursor + j + 1]));
+                    else
+                        tmp_data.push_back(_data[cursor + j + 1]);
+                }
+                cursor += num_links * 2 + 1;
+            }
+            std::vector<double> tmp_leg;
+            tmp_leg.reserve(30); // a randomly selected large number
+            for (int i = 0; i < tmp_num_legs - num_legs; ++i) {
+                generate_random_leg(tmp_leg);
+                tmp_data.insert(tmp_data.end(), tmp_leg.begin(), tmp_leg.end());
             }
         }
+        _data = tmp_data;
         _check_validity();
+    }
+
+    void generate_random_leg(std::vector<double>& leg_container) {
+        leg_container.clear();
+        int num_links = misc::rand<int>(min_num_links, max_num_links + 1);
+        leg_container.push_back(num_links);
+        for (int j = 0; j < num_links; ++j) {
+            // id and scale are up to phen to interpret or pass on to generator
+            leg_container.push_back(misc::rand<double>()); // link id
+            leg_container.push_back(misc::rand<double>()); // link scale
+        }
     }
 
     void cross(const EvoGenFloat& o, EvoGenFloat& c1, EvoGenFloat& c2) {
@@ -145,17 +203,13 @@ class EvoGenFloat {
         _data.push_back(misc::rand<double>()); // body_x
         _data.push_back(misc::rand<double>()); // body_y
         _data.push_back(misc::rand<double>()); // body_z
-        int num_links = 0;
-        int num_legs = misc::rand<int>(2, 4); // 2 or 3 legs
+        int num_legs = misc::rand<int>(min_num_legs, max_num_legs + 1);
         _data.push_back(num_legs);
+        std::vector<double> tmp_leg;
+        tmp_leg.reserve(30); // a randomly selected large number
         for (int i = 0; i < num_legs; ++i) {
-            num_links = misc::rand<int>(1, 4); // 1, 2 or 3 links per leg
-            _data.push_back(num_links);
-            for (int j = 0; j < num_links; ++j) {
-                // id and scale are up to phen to interpret or pass on to generator
-                _data.push_back(misc::rand<double>()); // link id
-                _data.push_back(misc::rand<double>()); // link scale
-            }
+            generate_random_leg(tmp_leg);
+            _data.insert(_data.end(), tmp_leg.begin(), tmp_leg.end());
         }
         _check_validity();
     }
