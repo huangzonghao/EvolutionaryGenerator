fc65ac56107329be3a67983757c3685ecf9a13cb
HuangZonghao <coding@hzh.io>
2021-08-15 14:58:22 -0400

    Update mesh converter to convert meshes for env

--- generator/src/mesh_converter_main.cpp
+++ generator/src/mesh_converter_main.cpp
@@ -16,7 +16,11 @@ void convert_stl_in_dir(const std::string& input_dir, const std::string& output_
     const std::string in_ext2(".STL");
     const std::string out_ext(".obj");
 
-    for (const auto& entry : std::filesystem::recursive_directory_iterator(input_path)) {
+    // relocate the converted mesh
+    bool converted_dir_not_created = true;
+    std::string converted_dir;
+
+    for (const auto& entry : std::filesystem::directory_iterator(input_path)) {
         const auto& tmp_path = entry.path();
         const auto& tmp_ext = tmp_path.extension().string();
         if (tmp_ext == in_ext || tmp_ext == in_ext2) {
@@ -24,15 +28,27 @@ void convert_stl_in_dir(const std::string& input_dir, const std::string& output_
             mesh->write(std::string(output_dir + "/" + tmp_path.stem().string() + out_ext).c_str());
             std::cout << tmp_path << " converted" << std::endl;
             delete mesh;
+            if (converted_dir_not_created) {
+                time_t t = time(0);
+                char time_buffer [80];
+                strftime(time_buffer, 80, "%Y%m%d_%H%M%S", localtime(&t));
+                converted_dir = input_dir + "/Converted/" + time_buffer;
+                std::filesystem::create_directories(converted_dir);
+                converted_dir_not_created = false;
+            }
+
+            std::filesystem::rename(tmp_path, converted_dir + "/" + tmp_path.filename().string());
         }
     }
-
 }
+
 int main(int argc, char *argv[]) {
     // convert body stl
-    convert_stl_in_dir(Robot_Output_Dir + "/tmp_robot_parts_stl/bodies", Robot_Parts_Dir + "/bodies");
+    convert_stl_in_dir(Mesh_Workspace_Dir + "/bodies", Robot_Parts_Dir + "/bodies");
     // convert leg stl
-    convert_stl_in_dir(Robot_Output_Dir + "/tmp_robot_parts_stl/legs", Robot_Parts_Dir + "/legs");
+    convert_stl_in_dir(Mesh_Workspace_Dir + "/legs", Robot_Parts_Dir + "/legs");
+    // convert env stl
+    convert_stl_in_dir(Mesh_Workspace_Dir + "/maps", EvoGen_Maps_Dir);
 
     std::cout << "Mesh_Converter done" << std::endl;
     return 0;
