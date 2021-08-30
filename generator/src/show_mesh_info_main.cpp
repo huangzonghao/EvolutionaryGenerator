91f0177f25b91f1bad4a99b7273aa8cef92f2a47
HuangZonghao <coding@hzh.io>
2021-08-30 14:00:09 -0400

    Fix show_mesh_info after mesh dir update

--- generator/src/show_mesh_info_main.cpp
+++ generator/src/show_mesh_info_main.cpp
@@ -1,7 +1,10 @@
 #include "MeshInfo.h"
+#include "evo_paths.h"
 
 extern MeshInfo mesh_info;
 int main(int argc, char *argv[]) {
+    mesh_info.set_mesh_dir(Robot_Parts_Dir);
+    mesh_info.init();
     mesh_info.print_all_size();
     return 0;
 }
