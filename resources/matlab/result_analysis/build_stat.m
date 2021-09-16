c72e28c8a4c6b3414a4950dc400b53c28333e6b6
HuangZonghao <coding@hzh.io>
2021-09-16 10:50:15 -0400

    Fix build stat

--- resources/matlab/result_analysis/build_stat.m
+++ resources/matlab/result_analysis/build_stat.m
@@ -1,4 +1,4 @@
-function [stat, stat_loaded] = build_stat(evo_params, orig_stat, orig_stat_loaded)
+function [stat, stat_loaded] = build_stat(result_path, evo_params, orig_stat, orig_stat_loaded)
     if (orig_stat_loaded)
         stat = orig_stat;
     else
@@ -11,7 +11,7 @@ function [stat, stat_loaded] = build_stat(evo_params, orig_stat, orig_stat_loade
     i_start = length(stat.archive_fits);
     stat_loaded = orig_stat_loaded;
 
-    stat_file = fullfile(evo_params.result_path, 'stat.mat');
+    stat_file = fullfile(result_path, 'stat.mat');
 
     nb_gen = evo_params.nb_gen;
     wb = waitbar(double(i_start + 1) / double(nb_gen + 1), ['Processing 1 / ', num2str(nb_gen + 1)], 'Name', 'Building Statistics ...');
@@ -19,12 +19,12 @@ function [stat, stat_loaded] = build_stat(evo_params, orig_stat, orig_stat_loade
         if mod(i, 10) == 0
             waitbar(double(i + 1) / double(nb_gen + 1), wb, ['Processing ', num2str(i + 1), ' / ', num2str(nb_gen + 1)]);
         end
-        curr_gen_archive = readmatrix(fullfile(evo_params.result_path, strcat('/archives/archive_', num2str(i), '.csv')));
+        curr_gen_archive = readmatrix(fullfile(result_path, strcat('/archives/archive_', num2str(i), '.csv')));
         fitness = curr_gen_archive(:, 4);
         stat.archive_fits(i + 1) = mean(fitness);
         max10_fitness = maxk(fitness, ceil(length(fitness) * 0.1));
         stat.elite_archive_fits(i + 1) = mean(max10_fitness);
-        current_gen_pop = readmatrix(fullfile(evo_params.result_path, strcat('/all_robots/', num2str(i), '.csv')));
+        current_gen_pop = readmatrix(fullfile(result_path, strcat('/all_robots/', num2str(i), '.csv')));
         pop_fitness = current_gen_pop(:, 2);
         stat.population_fits(i + 1) = mean(pop_fitness);
         stat.coverage(i + 1) = length(fitness) / (evo_params.griddim_0 * evo_params.griddim_1);
