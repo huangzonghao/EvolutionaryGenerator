0cb4272deb5826b852e7811c8e86b64a8fbe0df2
HuangZonghao <coding@hzh.io>
2021-10-13 03:50:32 -0400

    Replace rapidjson with nlohmann_json for better json obj management

--- generator/src/process_user_inputs_main.cpp
+++ generator/src/process_user_inputs_main.cpp
@@ -5,11 +5,7 @@
 #include <sstream>
 #include <filesystem>
 #include <thread>
-
-#define RAPIDJSON_HAS_STDSTRING 1
-#include <rapidjson/document.h>
-#include <rapidjson/filewritestream.h>
-#include <rapidjson/prettywriter.h>
+#include <nlohmann/json.hpp>
 
 #include "SimulatorParams.h"
 #include "SimulationManager.h"
@@ -20,16 +16,19 @@
 
 #include "evo_paths.h"
 
+using json = nlohmann::json;
 typedef sferes::fit::UrdfFitness fit_t;
 typedef sferes::phen::EvoGenPhen<sferes::gen::EvoGenFloat, fit_t> phen_t;
 
+// TODO: find a way to directly convert and include this struct to json obj
+// probably implement a nlohmann_json parser for it
 class EvalReport {
   public:
     double fitness = 0;
 };
 
 void eval_kernel(const std::vector<std::filesystem::path>& design_files,
-                 std::vector<rapidjson::Document>& jdocs,
+                 std::vector<json>& jsobjs,
                  std::vector<EvalReport>& reports,
                  const EvoParams& evo_params,
                  SimulatorParams sim_params,
@@ -60,25 +59,17 @@ void eval_kernel(const std::vector<std::filesystem::path>& design_files,
     sm.SetVisualization(false);
 
     for (int i = start_idx; i < end_idx; ++i) {
-        auto& jdoc = jdocs[i];
+        auto& jsobj = jsobjs[i];
         auto& report = reports[i];
 
-        std::ifstream infile(design_files[i]);
-        std::stringstream ss;
-        ss << infile.rdbuf();
-        jdoc.Parse(ss.str().c_str());
-        const rapidjson::Value& js_gene = jdoc["gene"];
-        std::vector<double> gene;
-        gene.resize(js_gene.Size());
-        for (int i = 0; i < gene.size(); ++i) {
-            gene[i] = js_gene[i].GetDouble();
-        }
+        std::ifstream ifs(design_files[i]);
+        jsobj = json::parse(ifs);
 
-        phen_t phen(gene, evo_params.phen_data_min(), evo_params.phen_data_max());
+        phen_t phen(jsobj["gene"], evo_params.phen_data_min(), evo_params.phen_data_max());
         phen.develop();
         const auto& robot = phen.get_robot();
 
-        sim_params.SetEnv(jdoc["environment"].GetString());
+        sim_params.SetEnv(jsobj["environment"]);
         sm.SetEnv(sim_params.GetEnv(),
                   sim_params.env_dim[0],
                   sim_params.env_dim[1],
@@ -110,7 +101,7 @@ void process_user_inputs(const std::filesystem::path& dir_path) {
         }
     }
     std::vector<EvalReport> reports(design_files.size());
-    std::vector<rapidjson::Document> jdocs(design_files.size());
+    std::vector<json> jsobjs(design_files.size());
 
     EvoParams evo_params;
     evo_params.Load(EvoGen_Params_Dir + "/evo_params.xml");
@@ -124,14 +115,13 @@ void process_user_inputs(const std::filesystem::path& dir_path) {
     int num_leftovers = design_files.size() - batch_size * num_threads;
     int element_cursor = 0;
     std::vector<std::shared_ptr<std::thread>> threads(num_threads);
-    // TODO: merge the following two for loops into one
     for (int i = 0; i < num_leftovers; ++i) {
-        threads[i] = std::make_shared<std::thread>(eval_kernel, std::cref(design_files), std::ref(jdocs), std::ref(reports),
+        threads[i] = std::make_shared<std::thread>(eval_kernel, std::cref(design_files), std::ref(jsobjs), std::ref(reports),
                                                    std::ref(evo_params), sim_params, element_cursor, element_cursor + batch_size + 1);
         element_cursor += batch_size + 1;
     }
     for (int i = num_leftovers; i < num_threads; ++i) {
-        threads[i] = std::make_shared<std::thread>(eval_kernel, std::cref(design_files), std::ref(jdocs), std::ref(reports),
+        threads[i] = std::make_shared<std::thread>(eval_kernel, std::cref(design_files), std::ref(jsobjs), std::ref(reports),
                                                    std::ref(evo_params), sim_params, element_cursor, element_cursor + batch_size);
         element_cursor += batch_size;
     }
@@ -139,60 +129,20 @@ void process_user_inputs(const std::filesystem::path& dir_path) {
     for (auto& thread : threads)
         thread->join();
 
-    rapidjson::Document output_jdoc;
-    output_jdoc.SetObject(); // without this you won't be able to add member to the dom tree
-    auto& output_allocator = output_jdoc.GetAllocator();
+    json output_jsobj;
 
     // Add meta info
-    // TODO: The following four lines to set up the user id in the new document is
-    // simply too complicated and stupid
-    std::string user_id = jdocs[0]["user_id"].GetString();
-    rapidjson::Value user_id_v(rapidjson::kObjectType);
-    user_id_v.SetString(rapidjson::StringRef(user_id.c_str()));
-    output_jdoc.AddMember("user_id", user_id_v, output_allocator);
+    output_jsobj["user_id"] = jsobjs[0]["user_id"];
 
-    rapidjson::Value ground_g(rapidjson::kObjectType);
-    rapidjson::Value sine_g(rapidjson::kObjectType);
-    rapidjson::Value valley_g(rapidjson::kObjectType);
     for (int i = 0; i < design_files.size(); ++i) {
-        auto& jdoc = jdocs[i];
-        // TODO: figure out a way to automatically generate env groups
-        // const auto& jdoc = jdocs[i];
-        // const auto& env = jdoc["environment"];
-        // std::string env_str = env.GetString();
-        // if (!output_jdoc.HasMember(env)) {
-            // output_jdoc.AddMember(env_str, rapidjson::Value(rapidjson::kObjectType), output_allocator);
-        // }
-        // auto& env_group = output_jdoc[env];
-        // env_group.AddMember(jdoc["ver"], jdoc["gene"], output_allocator);
-        rapidjson::Value fit_v(rapidjson::kObjectType);
-        fit_v.SetDouble(reports[i].fitness);
-        jdoc.AddMember("fitness", fit_v, jdocs[i].GetAllocator());
-        // auto key = rapidjson::Value(std::to_string(i), output_allocator); // need to do this due to stupid rapidjson design https://github.com/Tencent/rapidjson/issues/261
-        // output_jdoc.AddMember(key, jdocs[i], output_allocator);
-        std::string env = jdoc["environment"].GetString();
-
-        // Remove uncessary fields
-        auto ver_key = rapidjson::Value(std::to_string(jdoc["ver"].GetInt()), output_allocator); // need to do this due to stupid rapidjson design https://github.com/Tencent/rapidjson/issues/261
-        if (env == "ground") {
-            ground_g.AddMember(ver_key, jdoc, output_allocator);
-        } else if (env == "Sine2.obj") {
-            sine_g.AddMember(ver_key, jdoc, output_allocator);
-        } else if (env == "Valley5.obj") {
-            valley_g.AddMember(ver_key, jdoc, output_allocator);
-        }
+        auto& jsobj = jsobjs[i];
+        auto& tmp_jsobj = output_jsobj["designs"][jsobj["environment"].get<std::string>()][std::to_string(jsobj["ver"].get<int>())];
+        tmp_jsobj["gene"] = jsobj["gene"];
+        tmp_jsobj["fitness"] = reports[i].fitness;
     }
-    output_jdoc.AddMember("ground", ground_g, output_allocator);
-    output_jdoc.AddMember("Sine2.obj", sine_g, output_allocator);
-    output_jdoc.AddMember("Valley5.obj", valley_g, output_allocator);
-
-    rapidjson::StringBuffer strbuf;
-    rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(strbuf);
-    writer.SetFormatOptions(rapidjson::kFormatSingleLineArray);
-    output_jdoc.Accept(writer);
 
     std::ofstream ofs(User_Input_Dir + "/" + dir_path.stem().string() + ".json");
-    ofs << strbuf.GetString() << std::endl;
+    ofs << output_jsobj.dump() << std::endl; // the number in json::dump specifies indention
     ofs.close();
 }
 
