7be90c2f78b90bcd2ce42ff2424b51165a6fa739
HuangZonghao <coding@hzh.io>
2021-10-24 23:22:10 -0400

    Plot the ver fitness for multiple users together

--- resources/matlab/user_input_analysis/plot_ver_fitness.m
+++ resources/matlab/user_input_analysis/plot_ver_fitness.m
@@ -1,14 +1,37 @@
 function plot_ver_fitness(app)
-    if length(app.ListBox.Value) > 1
-        msgbox("Select one user only to generate the fitness plot", "Error");
+    num_results = length(app.ListBox.Value);
+    if num_results == 0
+        msgbox("Select at least one user to generate the fitness plot", "Error");
         return
     end
-    result = app.results{app.ListBox.Value};
+    if num_results < 5
+        num_cols = num_results;
+    else
+        num_cols = 5;
+    end
+
+    num_rows = ceil(num_results / num_cols);
+    new_fig = figure();
+    p = panel(new_fig);
+    p.pack(num_rows, num_cols)
+    counter = 1;
+    for nr = 1 : num_rows
+        for nc = 1 : num_cols
+            if counter > num_results
+                return
+            end
+            plot_ver_fitness_kernel(app, p(nr, nc), app.ListBox.Value(counter));
+            counter = counter + 1;
+        end
+    end
+end
+
+function plot_ver_fitness_kernel(app, p, result_id)
+    result = app.results{result_id};
     if result.num_env > length(app.default_env_order)
         fprintf("plot_ver_fitness Error: More env(%d) in %s than app.default_env_order(%d)\n", result.num_env, result.user_id, length(app.default_env_order));
     end
-    figure();
-    num_subplots = result.num_env;
+    p.pack(result.num_env,1);
     % Note the corner case : result.num_env < length(app.default_env_order)
     env_default_idx = []; % stores the id of the env in app.default_env_order
     user_study_order = []; % stores the place of the env in user study
@@ -30,13 +53,16 @@ function plot_ver_fitness(app)
 
     for i = 1 : result.num_env
         idx = env_default_idx(i);
-        subplot(result.num_env, 1, i);
+        % subplot(result.num_env, 1, i);
+        p(i, 1).select();
         plot(result.fitness(idx, :));
         env = app.default_env_order(idx);
+        xlim([1, 11]); % TODO: hard coded here
+        % ylim([]) % TODO: how to make the ylim be the same through all users of the same env?
         title(sprintf('%s - (%d)', app.default_env_order(idx), user_study_order(i)));
-        xlabel('ver');
-        ylabel('fitness');
     end
+    p.xlabel(['ver - ', result.user_id]);
+    p.ylabel(['fitness - ', result.user_id]);
 
     %% Helper functions
     function id = find_default_idx(env_name)
@@ -48,14 +74,13 @@ function plot_ver_fitness(app)
         id = -1;
         fprintf("find_default_idx Error: couldn't find %s in default env order", env_name);
     end
-    function p = find_place_in_user_study(env_name, result)
-        for p = 1 : length(result.envs)
-            if env_name == result.envs(p) % both are strings
+    function place = find_place_in_user_study(env_name, result)
+        for place = 1 : length(result.envs)
+            if env_name == result.envs(place) % both are strings
                 return
             end
         end
-        p = -1;
+        place = -1;
         fprintf("find_place_in_user_study Error: %s didn't show up in the user study %s", env_name, result.user_id);
     end
-    sgtitle(sprintf("Input of User %s - %s", result.user_id, order_name));
 end
