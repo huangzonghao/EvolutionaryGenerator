455f19dda9470a0c6a17cb82da2fb93763992c63
HuangZonghao <coding@hzh.io>
2021-10-29 10:33:43 -0400

    Update pop ver

--- resources/matlab/user_input_analysis/user_analysis_pop_var.m
+++ resources/matlab/user_input_analysis/user_analysis_pop_var.m
@@ -1,5 +1,6 @@
 % Pop variables to the base workspace for debugging
 function user_analysis_pop_var(app)
     assignin('base', 'user_analysis_app', app);
+    assignin('base', 'app', app);
     disp('Var poped to base workspace');
 end
