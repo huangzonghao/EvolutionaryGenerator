ce8a2fd7f7039886ff390733c125a750b94a4ded
HuangZonghao <coding@hzh.io>
2021-11-15 14:50:21 -0500

    Compute robot ages when building stat

--- resources/matlab/result_analysis/build_stat.m
+++ resources/matlab/result_analysis/build_stat.m
@@ -38,13 +38,17 @@ function [stat, stat_loaded] = build_stat(result_path, evo_params, orig_stat, or
         stat = orig_stat;
     else
         stat.archive_fits = zeros(1, nb_gen + 1);
+        stat.archive_age = zeros(1, nb_gen + 1);
         stat.elite_archive_fits = zeros(1, nb_gen + 1); % mean fitness of top 10% indivs of archive after each generation
+        stat.elite_archive_age = zeros(1, nb_gen + 1); % mean fitness of top 10% indivs of archive after each generation
         stat.population_fits = zeros(1, nb_gen + 1);
         stat.coverage = zeros(1, nb_gen + 1);
         stat.map_stat = zeros(evo_params.griddim_0, evo_params.griddim_1, nb_gen + 1);
 
         stat.clean_archive_fits = zeros(1, nb_gen + 1);
+        stat.clean_archive_age = zeros(1, nb_gen + 1);
         stat.clean_elite_archive_fits = zeros(1, nb_gen + 1); % mean fitness of top 10% indivs of archive after each generation
+        stat.clean_elite_archive_age = zeros(1, nb_gen + 1);
     end
 
     i_start = 0;
@@ -64,17 +68,25 @@ function [stat, stat_loaded] = build_stat(result_path, evo_params, orig_stat, or
         % Load gridmaps
         % gridmap format : gen_id, id, f_id1, f_id2, fitness
         curr_gen_archive = readmatrix(fullfile(result_path, strcat('/gridmaps/', num2str(i), '.csv')), delimitedTextImportOptions('DataLines',[1,Inf]), 'OutputType','double');
+
         fitness = curr_gen_archive(:, 5);
+        age = double(i) * ones(size(curr_gen_archive, 1), 1) - curr_gen_archive(:, 1);
         clean_fitness = curr_gen_archive(curr_gen_archive(:,3) ~= 0, 5);
+        clean_age = curr_gen_archive(curr_gen_archive(:,3) ~= 0, 1);
+        clean_age = double(i) * ones(size(clean_age)) - clean_age;
+
         stat.archive_fits(i + 1) = mean(fitness);
-        stat.clean_archive_fits(i + 1) = mean(clean_fitness);
-        max10_fitness = maxk(fitness, ceil(length(fitness) * 0.1));
+        stat.archive_age(i + 1) = mean(age);
+        [max10_fitness, max10_idx] = maxk(fitness, ceil(length(fitness) * 0.1));
         stat.elite_archive_fits(i + 1) = mean(max10_fitness);
-        clean_max10_fitness = maxk(clean_fitness, ceil(length(clean_fitness) * 0.1));
+        stat.elite_archive_age(i + 1) = mean(age(max10_idx));
+
+        stat.clean_archive_fits(i + 1) = mean(clean_fitness);
+        stat.clean_archive_age(i + 1) = mean(age);
+        [clean_max10_fitness, clean_max10_idx] = maxk(clean_fitness, ceil(length(clean_fitness) * 0.1));
         stat.clean_elite_archive_fits(i + 1) = mean(clean_max10_fitness);
-        current_gen_pop = readmatrix(fullfile(result_path, strcat('/robots/', num2str(i), '.csv')), delimitedTextImportOptions('DataLines',[1,Inf]), 'OutputType','double');
-        pop_fitness = current_gen_pop(:, 11);
-        stat.population_fits(i + 1) = mean(pop_fitness);
+        stat.clean_elite_archive_age(i + 1) = mean(clean_age(clean_max10_idx));
+
         stat.coverage(i + 1) = length(fitness) / archive_size;
         archive{i + 1} = curr_gen_archive;
 
@@ -92,6 +104,8 @@ function [stat, stat_loaded] = build_stat(result_path, evo_params, orig_stat, or
         %     in the training code that caused the results to be recorded in the randomized parent order
         curr_gen_robot = sortrows(curr_gen_robot, 2);
         robots(:, 1:9, i + 1) = curr_gen_robot(:, 3:11);
+        pop_fitness = curr_gen_robot(:, 11);
+        stat.population_fits(i + 1) = mean(pop_fitness);
 
         if stat.has_parentage
             % Update parentage
