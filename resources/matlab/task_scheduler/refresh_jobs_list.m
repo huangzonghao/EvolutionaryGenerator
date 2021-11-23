a76c8aa85a9124eab42b205cc578eeb4b6dcd64f
HuangZonghao <coding@hzh.io>
2021-11-23 02:51:00 -0500

    Show nickname of scheduled jobs in the job list box

--- resources/matlab/task_scheduler/refresh_jobs_list.m
+++ resources/matlab/task_scheduler/refresh_jobs_list.m
@@ -1,7 +1,11 @@
 function refresh_jobs_list(app)
     app.JobsListBox.Items = {};
     for i = 1 : length(app.jobs)
-        app.JobsListBox.Items{i} = app.jobs{i}.bagfile;
+        if ~isempty(app.jobs{i}.nickname)
+            app.JobsListBox.Items{i} = app.jobs{i}.nickname;
+        else
+            app.JobsListBox.Items{i} = app.jobs{i}.bagfile;
+        end
         app.JobsListBox.ItemsData(i) = i;
     end
 end
