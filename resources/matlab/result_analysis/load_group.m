06c22f03d13fb1e91497013f0bae72ceea17b2bf
HuangZonghao <coding@hzh.io>
2021-11-29 03:09:07 -0500

    Gracefully exit load group when no dir selected

--- resources/matlab/result_analysis/load_group.m
+++ resources/matlab/result_analysis/load_group.m
@@ -1,5 +1,8 @@
 function load_group(app)
     app.result_group_path = uigetdir(app.evogen_results_path, 'EvoGen Result Group Dir');
+    if app.result_group_path == 0
+        return
+    end
     figure(app.MainFigure);
     refresh_result_list(app);
 end
