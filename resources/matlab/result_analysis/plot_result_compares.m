5dbabe9933fce77e14dc87d3ee4a512f5ff38719
HuangZonghao <coding@hzh.io>
2021-12-01 15:20:12 -0500

    Plot parentage in result compare

--- resources/matlab/result_analysis/plot_result_compares.m
+++ resources/matlab/result_analysis/plot_result_compares.m
@@ -1,5 +1,5 @@
 function plot_result_compares(app, do_clean_plot)
-    num_subplots = 3;
+    num_subplots = 4;
     figure('units','normalized','outerposition',[.05 .05 .9 .9]);
     if ~isempty(app.CompPlotNameField.Value)
         tmp_title = app.CompPlotNameField.Value;
@@ -8,29 +8,33 @@ function plot_result_compares(app, do_clean_plot)
         end
         sgtitle(tmp_title, 'Interpreter', 'none');
     end
-    p1 = subplot(num_subplots, 1, 1);
-    hold on;
+    p1 = subplot(num_subplots, 1, 1, 'NextPlot', 'add');
     if do_clean_plot
-        title('Clean Fitness');
+        title(p1, 'Clean Fitness');
     else
-        title('Fitness');
+        title(p1, 'Fitness');
     end
-    xlabel('generation');
-    ylabel('fitness');
-    p2 = subplot(num_subplots, 1, 2);
-    hold on;
+    xlabel(p1, 'generation');
+    ylabel(p1, 'fitness');
+
+    p2 = subplot(num_subplots, 1, 2, 'NextPlot', 'add');
     if do_clean_plot
-        title('Clean Elite Fitness');
+        title(p2, 'Clean Elite Fitness');
     else
-        title('Elite Fitness');
+        title(p2, 'Elite Fitness');
     end
-    xlabel('generation');
-    ylabel('fitness');
-    p3 = subplot(num_subplots, 1, 3);
-    hold on;
-    title('Coverage');
-    xlabel('generation');
-    ylabel('coverage');
+    xlabel(p2, 'generation');
+    ylabel(p2, 'fitness');
+
+    p3 = subplot(num_subplots, 1, 3, 'NextPlot', 'add');
+    title(p3, 'Coverage');
+    xlabel(p3, 'generation');
+    ylabel(p3, 'coverage');
+
+    p4 = subplot(num_subplots, 1, 4, 'NextPlot', 'add');
+    title(p4, 'Average Parentage Percentage Per Robot');
+    xlabel(p4, 'generation');
+    ylabel(p4, 'parentage');
 
     % get each plot a different color
     plot_colors = 'brcmgk';
@@ -60,33 +64,43 @@ function plot_result_compares(app, do_clean_plot)
                     plot(p2, stat.elite_archive_fits, 'Color', plot_color, 'DisplayName', legend_name);
                 end
                 plot(p3, stat.coverage, 'Color', plot_color, 'DisplayName', legend_name);
+                if stat.has_parentage
+                    plot(p4, stat.archive_parentage, 'Color', plot_color, 'DisplayName', legend_name);
+                end
             end
         else % group
-            stat.coverage = [];
-            stat.archive_fits = [];
-            stat.elite_archive_fits = [];
-            stat.clean_archive_fits = [];
-            stat.clean_elite_archive_fits = [];
+            coverage = [];
+            archive_fits = [];
+            elite_archive_fits = [];
+            clean_archive_fits = [];
+            clean_elite_archive_fits = [];
+            archive_parentage = [];
 
             for i = 1 : length(result.result_full_paths)
                 [tmp_stat, tmp_stat_loaded] = load_stat(result.result_full_paths(i));
                 if (tmp_stat_loaded)
                     stat_loaded = true;
-                    stat.coverage(end + 1, :) = tmp_stat.coverage;
-                    stat.archive_fits(end + 1, :) = tmp_stat.archive_fits;
-                    stat.elite_archive_fits(end + 1, :) = tmp_stat.elite_archive_fits;
-                    stat.clean_archive_fits(end + 1, :) = tmp_stat.clean_archive_fits;
-                    stat.clean_elite_archive_fits(end + 1, :) = tmp_stat.clean_elite_archive_fits;
+                    coverage(end + 1, :) = tmp_stat.coverage;
+                    archive_fits(end + 1, :) = tmp_stat.archive_fits;
+                    elite_archive_fits(end + 1, :) = tmp_stat.elite_archive_fits;
+                    clean_archive_fits(end + 1, :) = tmp_stat.clean_archive_fits;
+                    clean_elite_archive_fits(end + 1, :) = tmp_stat.clean_elite_archive_fits;
+                    if tmp_stat.has_parentage
+                        archive_parentage(end + 1, :) = tmp_stat.archive_parentage;
+                    end
                 end
             end
             if do_clean_plot
-                shadedErrorBar(p1, [], stat.clean_archive_fits, {@mean, @std}, 'Color', plot_color, 'DisplayName', result.name);
-                shadedErrorBar(p2, [], stat.clean_elite_archive_fits, {@mean, @std}, 'Color', plot_color, 'DisplayName', result.name);
+                shadedErrorBar(p1, [], clean_archive_fits, {@mean, @std}, 'Color', plot_color, 'DisplayName', result.name);
+                shadedErrorBar(p2, [], clean_elite_archive_fits, {@mean, @std}, 'Color', plot_color, 'DisplayName', result.name);
             else
-                shadedErrorBar(p1, [], stat.archive_fits, {@mean, @std}, 'Color', plot_color, 'DisplayName', result.name);
-                shadedErrorBar(p2, [], stat.elite_archive_fits, {@mean, @std}, 'Color', plot_color, 'DisplayName', result.name);
+                shadedErrorBar(p1, [], archive_fits, {@mean, @std}, 'Color', plot_color, 'DisplayName', result.name);
+                shadedErrorBar(p2, [], elite_archive_fits, {@mean, @std}, 'Color', plot_color, 'DisplayName', result.name);
             end
-            shadedErrorBar(p3, [], stat.coverage, {@mean, @std}, 'Color', plot_color, 'DisplayName', result.name);
+            shadedErrorBar(p3, [], coverage, {@mean, @std}, 'Color', plot_color, 'DisplayName', result.name);
+            if length(archive_parentage) ~= 0
+                shadedErrorBar(p4, [], archive_parentage, {@mean, @std}, 'Color', plot_color, 'DisplayName', result.name);
+            end
         end
     end
 
@@ -94,6 +108,7 @@ function plot_result_compares(app, do_clean_plot)
         legend(p1, 'Interpreter', 'none');
         legend(p2, 'Interpreter', 'none');
         legend(p3, 'Interpreter', 'none', 'Location', 'SouthEast');
+        legend(p4, 'Interpreter', 'none', 'Location', 'SouthEast');
     end
     hold off;
 end
