5ad7850362aabb071bb1c5739e48c7d7609eddaa
HuangZonghao <coding@hzh.io>
2021-12-20 02:26:51 -0500

    Fix compare target deletion bug

--- resources/matlab/result_analysis/delete_from_compare_list.m
+++ resources/matlab/result_analysis/delete_from_compare_list.m
@@ -2,6 +2,7 @@ function delete_from_compare_list(app, do_remove_all)
     if do_remove_all
         app.targets_to_compare = {};
         app.CompareListBox.Items = {};
+        app.CompareListBox.ItemsData = [];
         return
     end
 
@@ -11,4 +12,5 @@ function delete_from_compare_list(app, do_remove_all)
 
     app.targets_to_compare(app.CompareListBox.Value) = [];
     app.CompareListBox.Items(app.CompareListBox.Value) = [];
+    app.CompareListBox.ItemsData(app.CompareListBox.Value) = [];
 end
