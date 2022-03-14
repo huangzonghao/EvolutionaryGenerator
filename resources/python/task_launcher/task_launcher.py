aef5426c72680f29b6731c4ecefb8ae165c92d41
HuangZonghao <coding@hzh.io>
2022-03-13 22:20:57 -0400

    Modularize python task launcher

--- resources/python/task_launcher/task_launcher.py
+++ resources/python/task_launcher/task_launcher.py
@@ -9,49 +9,55 @@ import sys
 import json
 import subprocess
 
-need_to_return = False
-evogen_exe_path = sys.argv[1];
-job_file = sys.argv[2]
-if not os.path.exists(evogen_exe_path):
-    print("Error: " + evogen_exe_path + " does not exist")
-    need_to_return = True
-if not os.path.exists(job_file):
-    print("Error: " + job_file + " does not exist")
-    need_to_return = True
-if need_to_return:
-    time.sleep(5) # let error message stay for a moment before cmd window is closed
-    exit()
+# set period to -1 to load period from job file
+def launch_trainer(evogen_exe_path, job_file_path, period):
+    need_to_return = False
+    if not os.path.exists(evogen_exe_path):
+        print("EvoGen Python Trainer Error: " + evogen_exe_path + " does not exist")
+        need_to_return = True
+    if not os.path.exists(job_file_path):
+        print("EvoGen Python Trainer Error: " + job_file_path + " does not exist")
+        need_to_return = True
+    if need_to_return:
+        time.sleep(5) # let error message stay for a moment before cmd window is closed
+        return
 
-period = 30 # default period is 30 mins
-# prioritize command line input over job file
-if len(sys.argv) > 3:
-    period = int(sys.argv[3])
-else:
-    fileobj = open(job_file)
-    jsobj = json.load(fileobj)
-    if 'session_time' in jsobj:
-        period = jsobj['session_time']
+    if period == -1: # no period given by user
+        fileobj = open(job_file_path)
+        jsobj = json.load(fileobj)
+        if 'session_time' in jsobj:
+            period = jsobj['session_time']
+        fileobj.close()
 
-print("EvoGen Python Trainer")
-print("Training with peroid of " + str(period) + " minutes\n\n")
+    print("EvoGen Python Trainer")
+    print("Training with peroid of " + str(period) + " minutes\n\n")
 
-num_class = 0
-while True:
-    print("================= Class " + str(num_class) + " =================")
-    print("Class time: " + str(period) + " mins\n")
-    try:
-        ret = subprocess.run([evogen_exe_path, 'job', job_file],
-                             timeout = period * 60, # timeout is in seconds
-                             stderr = subprocess.STDOUT)
+    num_class = 0
+    while True:
+        print("================= Class " + str(num_class) + " =================")
+        print("Class time: " + str(period) + " mins\n")
+        try:
+            ret = subprocess.run([evogen_exe_path, 'job', job_file_path],
+                                 timeout = period * 60, # timeout is in seconds
+                                 stderr = subprocess.STDOUT)
 
-        # if subprocess finished, all jobs has been done
-        if ret.returncode == 0:
-            print("All jobs has finished, return from task_launcher")
-            exit()
-    except subprocess.TimeoutExpired:
-        print("\nClass " + str(num_class) + " time's up")
-    except:
-        print("\nUnexcepted error")
-    print("\n\n--------------------------------------------")
-    print("Class " + str(num_class) + " finished\n\n")
-    num_class = num_class + 1
+            # if subprocess finished, all jobs have been done
+            if ret.returncode == 0:
+                print("\nEvogen Python Trainer: All jobs done. Return")
+                return
+        except subprocess.TimeoutExpired:
+            print("\nClass " + str(num_class) + " time's up")
+        except:
+            print("\nUnexcepted error")
+        print("\n\n--------------------------------------------")
+        print("Class " + str(num_class) + " finished\n\n")
+        num_class = num_class + 1
+
+if __name__ == "__main__":
+    evogen_exe_path = sys.argv[1]
+    job_file_path = sys.argv[2]
+    period = -1
+    if len(sys.argv) > 3:
+        period = int(sys.argv[3])
+
+    launch_trainer(evogen_exe_path, job_file_path, period)
