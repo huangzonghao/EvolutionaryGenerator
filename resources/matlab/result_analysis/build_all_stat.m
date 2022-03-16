b4c569f05ede462b3863baa11a0286b40d5dcb51
HuangZonghao <coding@hzh.io>
2022-03-15 21:14:24 -0400

    Add parent waitbar when building multiple results at the same time

--- resources/matlab/result_analysis/build_all_stat.m
+++ resources/matlab/result_analysis/build_all_stat.m
@@ -3,13 +3,17 @@ function build_all_stat(app)
     if length(app.results) == 0
         return
     end
-    for i = 1 : length(app.results)
+    num_results = length(app.results);
+    wb = waitbar(double(0), ['Processing 1 / ', num2str(num_results)], 'Name', 'Processing all results');
+    for i = 1 : num_results
         result_path = app.results{i}.path;
         if isfile(fullfile(result_path, 'stat.mat'));
             continue;
         end
         evo_params = load_evo_params(result_path);
-        build_stat(result_path, evo_params, [], false);
+        build_stat(result_path, evo_params, [], false, []);
+        waitbar(double(i + 1) / double(num_results), wb, sprintf("Processing %d / %d", i + 1, num_results));
     end
+    close(wb);
     refresh_result_list(app);
 end
--- resources/matlab/result_analysis/build_selected_stat.m
+++ resources/matlab/result_analysis/build_selected_stat.m
@@ -1,8 +1,12 @@
 function build_selected_stat(app)
-    for i = 1 : length(app.ResultsListBox.Value)
+    num_results = length(app.ResultsListBox.Value);
+    wb = waitbar(double(0), ['Processing 1 / ', num2str(num_results)], 'Name', 'Processing Selected results');
+    for i = 1 : num_results
         result_path = app.results{app.ResultsListBox.Value{i}}.path;
         evo_params = load_evo_params(result_path);
         build_stat(result_path, evo_params, [], false);
+        waitbar(double(i + 1) / double(num_results), wb, sprintf("Processing %d / %d", i + 1, num_results));
     end
+    close(wb);
     refresh_result_list(app);
 end
