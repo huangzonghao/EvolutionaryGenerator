f392abc2cb1b12e22c31f05888cce409c07444b2
HuangZonghao <coding@hzh.io>
2022-08-04 07:29:07 -0400

    Fix remove job in task scheduler

--- resources/matlab/task_scheduler/remove_job.m
+++ resources/matlab/task_scheduler/remove_job.m
@@ -1,4 +1,8 @@
 function remove_job(app)
+    if isempty(app.JobsListBox.Value)
+        msgbox('Select a job to remove');
+        return
+    end
     app.jobs(app.JobsListBox.Value) = [];
     refresh_jobs_list(app);
 end
