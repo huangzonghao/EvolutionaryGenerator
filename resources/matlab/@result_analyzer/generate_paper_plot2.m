8c8bd2d6b4525590e2d6b134b27d80260711b794
HuangZonghao <coding@hzh.io>
2022-11-10 16:16:47 -0500

    Enable boxplot for varying number of data of each group

--- resources/matlab/@result_analyzer/generate_paper_plot2.m
+++ resources/matlab/@result_analyzer/generate_paper_plot2.m
@@ -30,22 +30,22 @@ function generate_paper_plot2(app)
     end
 
     % Final QD-Score
-    qd_score_mat = [];
-    global_performance_mat = [];
-    reliability_mat = [];
+    qd_score_mat = {};
+    global_performance_mat = {};
+    reliability_mat = {};
     for i_virtual = 1 : num_virtual_results
         virtual_result = app.virtual_results{app.targets_to_compare{i_virtual}.id};
         num_results = virtual_result.num_results;
-        qd_column = zeros(num_results, 1);
-        global_performance_column = zeros(num_results, 1);
-        reliability_column = zeros(num_results, 1);
+        qd_tmp = zeros(1, num_results);
+        global_performance_tmp = zeros(1, num_results);
+        reliability_tmp = zeros(1, num_results);
 
         for i_result = 1 : num_results
             % get the qd score for each result
             result = load_target_result(app, false, virtual_result.ids(i_result));
 
-            qd_column(i_result) = result.stat.qd_score(end);
-            global_performance_column(i_result) = result.stat.best_fits(end);
+            qd_tmp(i_result) = result.stat.qd_score(end);
+            global_performance_tmp(i_result) = result.stat.best_fits(end);
 
             current_gen_archive = result.archive{result.evo_params.nb_gen};
             if result.version < 2
@@ -73,12 +73,12 @@ function generate_paper_plot2(app)
             % sanitize the second dimension (here grid_dim(1) gives the size of first dimension)
             fitness(sub2ind(size(archive_map), 1:grid_dim(1), ones(1, grid_dim(1)))) = 0.1 * rand(grid_dim(1), 1) + fitness(sub2ind(size(archive_map), 1:grid_dim(1), 1 + ones(1, grid_dim(1))));
             archive_map(sub2ind(size(archive_map), f_ids(:, 1), f_ids(:, 2))) = fitness;
-            reliability_column(i_result) = sum(sum(archive_map ./ virtual_result.benchmark_archive)) / length(archive_map(:));
+            reliability_tmp(i_result) = sum(sum(archive_map ./ virtual_result.benchmark_archive)) / length(archive_map(:));
             f_ids = [];
         end
-        qd_score_mat = [qd_score_mat qd_column];
-        global_performance_mat = [global_performance_mat global_performance_column / virtual_result.benchmark_best_fit];
-        reliability_mat = [reliability_mat reliability_column];
+        qd_score_mat{i_virtual} = qd_tmp;
+        global_performance_mat{i_virtual} = global_performance_tmp;
+        reliability_mat{i_virtual} = reliability_tmp;
     end
 
     p_qdscore = [];
@@ -86,25 +86,41 @@ function generate_paper_plot2(app)
     p_reliability = [];
     for i = 1 : 4
         for j = i + 1 : 5
-            [p_qdscore(end+1), ~] = ranksum(qd_score_mat(:,i), qd_score_mat(:,j));
-            [p_globalperformance(end+1), ~] = ranksum(global_performance_mat(:,i), global_performance_mat(:,j));
-            [p_reliability(end+1), ~] = ranksum(reliability_mat(:,i), reliability_mat(:,j));
+            [p_qdscore(end+1), ~] = ranksum(qd_score_mat{i}, qd_score_mat{j});
+            [p_globalperformance(end+1), ~] = ranksum(global_performance_mat{i}, global_performance_mat{j});
+            [p_reliability(end+1), ~] = ranksum(reliability_mat{i}, reliability_mat{j});
         end
     end
 
-    boxplot(p{1}, qd_score_mat, 'Labels', {'H0', 'H5', 'H15', 'H25', 'H30'});
+    qd_score_data = [];
+    qd_score_group = [];
+    global_performance_data = [];
+    global_performance_group = [];
+    reliability_data = [];
+    reliability_group = [];
+    for i = 1 : num_virtual_results
+        qd_score_data = [qd_score_data qd_score_mat{i}];
+        qd_score_group = [qd_score_group i * ones(size(qd_score_mat{i}))];
+        global_performance_data = [global_performance_data global_performance_mat{i}];
+        global_performance_group = [global_performance_group i * ones(size(global_performance_mat{i}))];
+        reliability_data = [reliability_data reliability_mat{i}];
+        reliability_group = [reliability_group i * ones(size(reliability_mat{i}))];
+    end
+    qd_score_data
+    qd_score_group
+    boxplot(p{1}, qd_score_data, qd_score_group, 'Labels', {'H0', 'H5', 'H15', 'H25', 'H30'});
     title(p{1}, 'QD-Score', 'FontName', 'Times New Roman', 'FontWeight', 'bold', 'FontSize', 11);
     ylabel(p{1}, 'QD-Score', 'FontName', 'Times New Roman', 'FontWeight', 'bold', 'FontSize', 11);
     axes(p{1});
     sigstar(sigstar_groups, p_qdscore);
 
-    boxplot(p{2}, global_performance_mat, 'Labels', {'H0', 'H5', 'H15', 'H25', 'H30'});
+    boxplot(p{2}, global_performance_data, global_performance_group, 'Labels', {'H0', 'H5', 'H15', 'H25', 'H30'});
     title(p{2}, 'Global Performance', 'FontName', 'Times New Roman', 'FontWeight', 'bold', 'FontSize', 11);
     ylabel(p{2}, 'Global Performance', 'FontName', 'Times New Roman', 'FontWeight', 'bold', 'FontSize', 11);
     axes(p{2});
     sigstar(sigstar_groups, p_globalperformance);
 
-    boxplot(p{3}, reliability_mat, 'Labels', {'H0', 'H5', 'H15', 'H25', 'H30'});
+    boxplot(p{3}, reliability_data, reliability_group, 'Labels', {'H0', 'H5', 'H15', 'H25', 'H30'});
     title(p{3}, 'Reliabilitiy / Precision', 'FontName', 'Times New Roman', 'FontWeight', 'bold', 'FontSize', 11);
     ylabel(p{3}, 'Reliabilitiy / Precision', 'FontName', 'Times New Roman', 'FontWeight', 'bold', 'FontSize', 11);
     axes(p{3});
