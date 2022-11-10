85bdf58095275bdf7efe58dbabf4b81c4d22b9fe
HuangZonghao <coding@hzh.io>
2022-11-10 16:31:11 -0500

    Update waitbar info to reflect the progress of each substep

--- resources/matlab/@result_analyzer/build_clean_pack_results.m
+++ resources/matlab/@result_analyzer/build_clean_pack_results.m
@@ -25,7 +25,7 @@ function build_clean_pack_results(app)
     wb = waitbar(double(0), ['Processing 1 / ', num2str(num_results)], 'Name', 'Processing all results');
     wb.Position(2) = wb.Position(2) - wb.Position(4);
     for i = 1 : num_results
-        waitbar(double(i) / double(num_results), wb, sprintf("Processing %d / %d - building", i, num_results));
+        waitbar(double(i - 1) / double(num_results), wb, sprintf("Processing %d / %d - building", i, num_results));
 
         result = app.results{result_idxs(i)};
         % build
@@ -36,16 +36,17 @@ function build_clean_pack_results(app)
         end
 
         % clean
-        wb.Children.Title.String = sprintf("Processing %d / %d - cleaning", i, num_results);
+        waitbar(double(i - 1 + 0.3) / double(num_results), wb, sprintf("Processing %d / %d - cleaning", i, num_results));
         app.clean_result(result);
 
         % export
+        % waitbar(double(i - 1 + 0.5) / double(num_results), wb, sprintf("Processing %d / %d - exporting", i, num_results));
         % wb.Children.Title.String = sprintf("Processing %d / %d - exporting", i, num_results);
         % app.export_result(result, export_dir);
 
         % pack
+        waitbar(double(i - 1 + 0.8) / double(num_results), wb, sprintf("Processing %d / %d - packing", i, num_results));
         % app.pack_result(fullfile(export_dir, result.basename));
-        wb.Children.Title.String = sprintf("Processing %d / %d - packing", i, num_results);
         app.pack_result(result.path);
     end
 
